6462aa14588f411bb572e819465a55d4c3cc768c
Hunter Miller <svarty1@gmail.com>
2020-05-04 20:54:08 -0500

    improving copy on intro

--- src/components/LetterGenerator.svelte
+++ src/components/LetterGenerator.svelte
@@ -15,19 +15,19 @@
 
 <form class="text-xl sm:text-2xl" on:submit|preventDefault={generate}>
     <div class="leading-looser mb-8">
-        If you could introduce yourself as
+        Please introduce yourself! You are
         <div class="relative inline leading-none">
             <input id="yourName" class="control w-40 sm:w-48 -mb-2 sm:py-1 px-2 text-center border-b-2" type="text" required bind:value={$theirName}>
             <label for="yourName" class="absolute left-0 inline-block w-full text-center font-semibold opacity-75">your name</label>
-        </div>, where you
+        </div>, a hiring manager at,
         <div class="relative inline leading-none">
             <input id="company" class="control w-40 sm:w-48 -mb-2 sm:py-1 px-2 text-center border-b-2" type="text" required bind:value={company}>
-            <label for="company" class="absolute left-0 inline-block w-full text-center font-semibold opacity-75">work</label>
-        </div>, and
+            <label for="company" class="absolute left-0 inline-block w-full text-center font-semibold opacity-75">company</label>
+        </div>, looking to hire a
         <div class="relative inline leading-none">
             <input id="position" class="control w-48 sm:w-64 -mb-2 sm:py-1 px-2 text-center border-b-2" type="text" required bind:value={job}>
             <label for="position" class="absolute left-0 inline-block w-full text-center font-semibold opacity-75">the position</label>
-        </div> you're hiring for, then we can move on to the letter. 
+        </div>.
     </div>
     <button class="button control">See the letter</button>
 </form>
